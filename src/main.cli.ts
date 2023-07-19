d3c133cd5805651e8bf4770e0882c0e77701ea7f
ikave <shiftgear@mail.ru>
2023-07-19 20:38:18 +0300

    Регистрирует команду генерации моковых данных, исправляет заготовки данных для json сервера

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import CLIAplication from './app/cli.js';
+import GenerateCommand from './core/cli-command/generate.command.js';
 import HelpCommand from './core/cli-command/help.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
 import VersionCommand from './core/cli-command/version.command.js';
@@ -9,5 +10,6 @@ cliManager.registerCommands([
   new HelpCommand(),
   new VersionCommand(),
   new ImportCommand(),
+  new GenerateCommand(),
 ]);
 cliManager.processCommand(process.argv);
