0aa7b74e75b11adb98b82cf33b07dec090c912c9
ikave <shiftgear@mail.ru>
2023-07-24 20:12:20 +0300

    Создает конфиг для env

---
+++ src/core/config/config.service.ts
@@ -0,0 +1,35 @@
+import { config } from 'dotenv';
+import { inject, injectable } from 'inversify';
+import { LoggerInterface } from '../logger/logger.interface.js';
+import { ConfigInterface } from './config.interface.js';
+import { RestSchema, configRestSchema } from './rest.schema.js';
+import { AppComponent } from '../../types/app-component.enum.js';
+
+@injectable()
+export default class ConfigService implements ConfigInterface<RestSchema> {
+  private readonly config: RestSchema;
+
+  constructor(
+    @inject(AppComponent.LoggerInterface)
+    private readonly logger: LoggerInterface
+  ) {
+    const parsedOutput = config();
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+
+    if (parsedOutput.error) {
+      throw new Error(
+        'Can not read .env file. Perhaps the file does not exists.'
+      );
+    }
+
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
+    return this.config[key];
+  }
+}
