d6e6e8e048959d425b24d2c9bde3cff7e71f8c69
ikave <shiftgear@mail.ru>
2023-07-24 20:08:53 +0300

    Добавляет схему конфига, добавляет перечисление компонентов

---
+++ src/core/config/rest.schema.ts
@@ -0,0 +1,31 @@
+import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
+
+export type RestSchema = {
+  PORT: number;
+  SALT: string;
+  DB_HOST: string;
+};
+
+export const configRestSchema = convict<RestSchema>({
+  PORT: {
+    doc: 'Port for incoming connections',
+    format: 'port',
+    env: 'PORT',
+    default: 3000,
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null,
+  },
+  DB_HOST: {
+    doc: 'IP address of the db server',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '172.0.0.1',
+  },
+});
