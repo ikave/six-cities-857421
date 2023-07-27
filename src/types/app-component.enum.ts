dcb436c23363d17c50814ccebef60cffead05962
ikave <shiftgear@mail.ru>
2023-07-27 13:56:28 +0300

    Добавляет сервис для подключениея к БД, устанавливает зависимости.

--- src/types/app-component.enum.ts
+++ src/types/app-component.enum.ts
@@ -2,4 +2,5 @@ export enum AppComponent {
   RestApplication = 'RestApplication',
   LoggerInterface = 'LoggerInterface',
   ConfigInterface = 'ConfigInterface',
+  DatabaseClientInterface = 'DatabaseClientInterface',
 }
