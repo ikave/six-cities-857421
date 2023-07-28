509c01c64dbe0c33dc733dc645d1c5111052dc0c
ikave <shiftgear@mail.ru>
2023-07-28 13:46:35 +0300

    Выделяет контейнер приложения в отдельный файл

---
+++ src/app/rest.container.ts
@@ -0,0 +1,36 @@
+import { Container } from 'inversify';
+import { AppComponent } from '../types/app-component.enum.js';
+import RestApplication from './rest.js';
+import { LoggerInterface } from '../core/logger/logger.interface.js';
+import PinoService from '../core/logger/pino.service.js';
+import { RestSchema } from '../core/config/rest.schema.js';
+import { ConfigInterface } from '../core/config/config.interface.js';
+import ConfigService from '../core/config/config.service.js';
+import { DatabaseClientInterface } from '../core/database-client/database-client.interface.js';
+import MongoClientService from '../core/database-client/mongo-client.service.js';
+
+export const createRestAppContainer = () => {
+  const container = new Container();
+
+  container
+    .bind<RestApplication>(AppComponent.RestApplication)
+    .to(RestApplication)
+    .inSingletonScope();
+
+  container
+    .bind<LoggerInterface>(AppComponent.LoggerInterface)
+    .to(PinoService)
+    .inSingletonScope();
+
+  container
+    .bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface)
+    .to(ConfigService)
+    .inSingletonScope();
+
+  container
+    .bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface)
+    .to(MongoClientService)
+    .inSingletonScope();
+
+  return container;
+};
