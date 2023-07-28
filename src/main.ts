2e8d95c3edbd4e87b16cd8c8a87ff65f6f8e1f79
ikave <shiftgear@mail.ru>
2023-07-28 13:48:00 +0300

    Объединяет контейнеры

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,15 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import RestApplication from './app/rest.js';
-import ConfigService from './core/config/config.service.js';
-import PinoService from './core/logger/pino.service.js';
 import { AppComponent } from './types/app-component.enum.js';
-import { LoggerInterface } from './core/logger/logger.interface.js';
-import { ConfigInterface } from './core/config/config.interface.js';
-import { RestSchema } from './core/config/rest.schema.js';
-import { DatabaseClientInterface } from './core/database-client/database-client.interface.js';
-import MongoClientService from './core/database-client/mongo-client.service.js';
+import { createRestAppContainer } from './app/rest.container.js';
+import { createUserContainer } from './modules/user/user.container.js';
 
 async function bootstrap() {
-  const container = new Container();
-
-  container
-    .bind<RestApplication>(AppComponent.RestApplication)
-    .to(RestApplication)
-    .inSingletonScope();
-
-  container
-    .bind<LoggerInterface>(AppComponent.LoggerInterface)
-    .to(PinoService)
-    .inSingletonScope();
-
-  container
-    .bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface)
-    .to(ConfigService)
-    .inSingletonScope();
-
-  container
-    .bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface)
-    .to(MongoClientService)
-    .inSingletonScope();
+  const container = Container.merge(
+    createRestAppContainer(),
+    createUserContainer()
+  );
 
   const application = container.get<RestApplication>(
     AppComponent.RestApplication
