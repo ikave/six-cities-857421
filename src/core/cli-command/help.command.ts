4b88ca3b78aae5b7d2f6d6aec1af34b4a93b3b5b
ikave <shiftgear@mail.ru>
2023-08-05 09:09:04 +0300

    fix: Исправляет пути при импорте, дополняет описание команды --import

--- src/core/cli-command/help.command.ts
+++ src/core/cli-command/help.command.ts
@@ -13,7 +13,7 @@ export default class HelpCommand implements CLICommandInterface {
 
       --version:                   # выводит номер версии
       --help:                      # печатает этот текст
-      --import <path>:             # импортирует данные из TSV
+      --import <path> <username> <password> <host> <dbname> <salt>:             # импортирует данные из TSV
       --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных`);
   }
 }
